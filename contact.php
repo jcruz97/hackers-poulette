0a01e962d0b47eb70c1d3c03e5593880d0003cbe
jcruz97 <cruzjurgen97@gmail.com>
2020-08-28 12:22:42 +0200

    first try live

---
+++ contact.php
@@ -0,0 +1,12 @@
+<?php
+$first_name = $_POST['fName'];
+$last_name = $_POST['lName'];
+$gender = $_POST['gender'];
+$email = $_POST['email'];
+$occupation = $_POST['occupation'];
+$country = $_POST['country'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+echo "Thank you for your message!" . " -" . "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> Return Home</a>";;
+?>
