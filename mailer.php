99dddd6c8c18fc10e62443736c60476e8138fc5e
jcruz97 <cruzjurgen97@gmail.com>
2020-08-28 16:11:36 +0200

    test

--- mailer.php
+++ mailer.php
@@ -6,7 +6,7 @@ $recipient = "$new_email";
 $subject = "Contact Form feedback";
 $mailheader = "From: $new_email";
 
-mail($recipient, $subject, $test_message, $mailheader) or die("Error!");
+mail($recipient, $subject, $test_message, $mailheader);
 
 echo "Thank you for your message!" . " -" . "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> Return Home</a>";
 ?>
