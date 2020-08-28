b0630ed939d45839f06111938dca82f6afd161ee
jcruz97 <cruzjurgen97@gmail.com>
2020-08-28 17:17:03 +0200

    17h15 push 28/08

--- filter.php
+++ filter.php
@@ -5,28 +5,28 @@ $new_firstname = filter_var($firstname, FILTER_SANITIZE_STRING);
 $lastname = $_POST['lName'];
 $new_lastname = filter_var($lastname, FILTER_SANITIZE_STRING);
 
-$gender = $_POST['gender'];
+$gender = $_POST['genderChoice'];
 $new_gender = filter_var($gender, FILTER_SANITIZE_STRING);
 
-$email = $_POST['email']; //Sanitizing and validate email
+$email = $_POST['emailAdress']; //Sanitizing and validate email
 $new_email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
 if (!filter_var($new_email, FILTER_VALIDATE_EMAIL) === false){
-    print_r($new_email);
+    //print_r($new_email);
 } else {
     $errors['new_email'] = 'Email is not valid!';
 }
 
-$occupation = $_POST['occupation'];
+$occupation = $_POST['occupationTask'];
 $new_occupation = filter_var($occupation, FILTER_SANITIZE_STRING);
 
-$country = $_POST['country'];
+$country = $_POST['countryName'];
 $new_country = filter_var($country, FILTER_SANITIZE_STRING);
 
-$subject = $_POST['subject'];
+$subject = $_POST['subjectChoice'];
 $new_subject = filter_var($subject, FILTER_SANITIZE_STRING);
 
-$message = $_POST['message'];
+$message = $_POST['messageAreaText'];
 $new_message = strip_tags($message);
 $clean_message = htmlentities($new_message, ENT_QUOTES, 'UTF-8');
 
--- mailer.php
+++ mailer.php
@@ -4,14 +4,11 @@ require "filter.php";
 $test_message = 'This is a test message for HP';
 $recipient = "$new_email";
 $subject = "Contact Form feedback";
-$mailheader = "From: $new_email";
+$mailheader = "From: $new_email . \r\n";
 
-$is_mail_sent = mail($recipient, $subject, $test_message, $mailheader);
+mail($recipient, $subject, $test_message, $mailheader);
 
-if ($is_mail_sent === true){
-    echo "Thank you for your message!" . "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' class='text-center' style='texte-decoration:none;color:#0d8187'> Return Home</a>";
-} else {
-    echo 'Error';
-}
+
+echo "Thank you for your message!" . "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' class='text-center' style='texte-decoration:none;color:#0d8187'> Return Home</a>";
 
 ?>
