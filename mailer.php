c3f4fac9fbc001416578f8f4ff618be8a8e04d9a
jcruz97 <cruzjurgen97@gmail.com>
2020-08-28 16:27:01 +0200

    First deloy heroku

--- mailer.php
+++ mailer.php
@@ -6,7 +6,12 @@ $recipient = "$new_email";
 $subject = "Contact Form feedback";
 $mailheader = "From: $new_email";
 
-mail($recipient, $subject, $test_message, $mailheader);
+$is_mail_sent = mail($recipient, $subject, $test_message, $mailheader);
 
-echo "Thank you for your message!" . "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' class='text-center' style='texte-decoration:none;color:#0d8187'> Return Home</a>";
+if ($is_mail_sent === true){
+    echo "Thank you for your message!" . "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' class='text-center' style='texte-decoration:none;color:#0d8187'> Return Home</a>";
+} else {
+    echo 'Error';
+}
+
 ?>
