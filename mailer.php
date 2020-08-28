a909d83873b627e89b696c746b072e59d2d581df
jcruz97 <cruzjurgen97@gmail.com>
2020-08-28 16:10:49 +0200

    test

--- mailer.php
+++ mailer.php
@@ -1,12 +1,12 @@
 <?php
 require "filter.php";
 
-$formcontent="From: $new_firstname + $new_lastname \n Message: $clean_message";
+$test_message = 'This is a test message for HP';
 $recipient = "$new_email";
 $subject = "Contact Form feedback";
-$mailheader = "From: $new_email \r\n";
+$mailheader = "From: $new_email";
 
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+mail($recipient, $subject, $test_message, $mailheader) or die("Error!");
 
 echo "Thank you for your message!" . " -" . "<a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> Return Home</a>";
 ?>
